88ef3ca0886b427b5bccb931eea2f8e8e01c5eb7
kashpan <infinityson41@email.com>
2026-05-07 17:18:16 +0300

    Fix database environment configuration

--- database.js
+++ database.js
@@ -1,16 +1,16 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    // This is the specific line that fixes the "manual install" error:
-    dialectModule: require("mysql2"),
-  },
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        port: process.env.DB_PORT,
+      },
+    );
 
 module.exports = sequelize;
